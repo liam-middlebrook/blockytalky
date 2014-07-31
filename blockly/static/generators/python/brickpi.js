40d08b9791e5298081a238868139bbb48821e85a
Liam Middlebrook <liammiddlebrook@gmail.com>
2014-07-31 15:44:44 -0400

    Fixed non-escaped double-quotes on line 381

--- blockly/static/generators/python/brickpi.js
+++ blockly/static/generators/python/brickpi.js
@@ -381,7 +381,7 @@ Blockly.Python.motor_servo = function() {
     code =  "initAngle = self.getSensorValue("encoder", ' + value_motor_number + ')" +'\n'
     code += "degrees = " + value_motor_angle + '\n'
     code += "motor = " + value_motor_number + '\n'
-    code += "diff = degrees - self.getSensorValue("encoder", motor)" + '\n'
+    code += "diff = degrees - self.getSensorValue(\"encoder\", motor)" + '\n'
     code += "while abs(diff)>5:" + '\n'
     code += '\t' + "power = diff" + '\n'
     code += '\t' + "SetMotor(motor, power)" + '\n'
