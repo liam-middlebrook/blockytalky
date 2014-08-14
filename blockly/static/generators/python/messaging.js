36d67aceb7e562f40d18007a79a1108b6e7994ca
Liam Middlebrook <liammiddlebrook@gmail.com>
2014-08-14 16:38:27 -0400

    Print current working directory of script

--- blockly/static/generators/python/messaging.js
+++ blockly/static/generators/python/messaging.js
@@ -225,7 +225,8 @@ Blockly.Python.send_osc= function() {
 
 Blockly.Python.facebook_post= function() {
     var content = this.getTitleValue('content');
-    var code= 'with open("~/blockytalky/backend/fb_config.yaml", "r") as myfile:' + '\n'
+    var code= 'print os.getcwd()' + '\n'
+    code += 'with open("~/blockytalky/backend/fb_config.yaml", "r") as myfile:' + '\n'
     code += '\t' + 'token = yaml.load(myfile.read())["token"]' + '\n'
     code += 'graph = GraphAPI(token)' + '\n'
     code += 'graph.post("me/feed", message="' + content + '")' +'\n'
