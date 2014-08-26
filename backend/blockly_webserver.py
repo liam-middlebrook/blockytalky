ac514a1b6f38ad4d7b29045829219444b49931f9
Sean Brennan <esean17@yahoo.com>
2014-08-25 21:43:59 -0400

    switched to another recommended cross-domain decorator

--- backend/blockly_webserver.py
+++ backend/blockly_webserver.py
@@ -90,7 +90,13 @@ def check_auth(username, password):
             bcrypt.check_password_hash(device_settings['password_hash'], password))
 
 # allow crossdomain access
-def crossdomain(origin=None, methods=None, headers=None, max_age=21600, attach_to_all=True, automatic_options=True):  
+from datetime import timedelta
+from flask import make_response, request, current_app
+from functools import update_wrapper
+
+def crossdomain(origin=None, methods=None, headers=None,
+                max_age=21600, attach_to_all=True,
+                automatic_options=True):
     if methods is not None:
         methods = ', '.join(sorted(x.upper() for x in methods))
     if headers is not None and not isinstance(headers, basestring):
@@ -117,10 +123,12 @@ def crossdomain(origin=None, methods=None, headers=None, max_age=21600, attach_t
                 return resp
 
             h = resp.headers
-
             h['Access-Control-Allow-Origin'] = origin
             h['Access-Control-Allow-Methods'] = get_methods()
             h['Access-Control-Max-Age'] = str(max_age)
+            h['Access-Control-Allow-Credentials'] = 'true'
+            h['Access-Control-Allow-Headers'] = \
+                "Origin, X-Requested-With, Content-Type, Accept, Authorization"
             if headers is not None:
                 h['Access-Control-Allow-Headers'] = headers
             return resp
