4349e0c4f1f368014e3a773d4fcee6c7c252009a
Sean Brennan <esean17@yahoo.com>
2014-08-25 20:56:30 -0400

    added a crossdomain decorator to allow external access to the blockly_webserver.

--- backend/blockly_webserver.py
+++ backend/blockly_webserver.py
@@ -21,6 +21,10 @@ from message import *
 import time, commands, subprocess, pika
 import jsonpickle
 
+# imports for the crossdomain decorator
+from datetime import timedelta
+from flask import make_response, current_app
+
 app = Flask(__name__)
 bcrypt = Bcrypt(app)
 logger = logging.getLogger(__name__)
@@ -83,6 +87,50 @@ def requires_auth(f):
 def check_auth(username, password):
     return (username == device_settings['device_name'] and
             bcrypt.check_password_hash(device_settings['password_hash'], password))
+
+# allow crossdomain access
+def crossdomain(origin=None, methods=None, headers=None,
+                max_age=21600, attach_to_all=True,
+                automatic_options=True):
+    if methods is not None:
+        methods = ', '.join(sorted(x.upper() for x in methods))
+    if headers is not None and not isinstance(headers, basestring):
+        headers = ', '.join(x.upper() for x in headers)
+    if not isinstance(origin, basestring):
+        origin = ', '.join(origin)
+    if isinstance(max_age, timedelta):
+        max_age = max_age.total_seconds()
+
+    def get_methods():
+        if methods is not None:
+            return methods
+
+        options_resp = current_app.make_default_options_response()
+        return options_resp.headers['allow']
+
+    def decorator(f):
+        def wrapped_function(*args, **kwargs):
+            if automatic_options and request.method == 'OPTIONS':
+                resp = current_app.make_default_options_response()
+            else:
+                resp = make_response(f(*args, **kwargs))
+            if not attach_to_all and request.method != 'OPTIONS':
+                return resp
+
+            h = resp.headers
+            h['Access-Control-Allow-Origin'] = origin
+            h['Access-Control-Allow-Methods'] = get_methods()
+            h['Access-Control-Max-Age'] = str(max_age)
+            h['Access-Control-Allow-Credentials'] = 'true'
+            h['Access-Control-Allow-Headers'] = \
+                "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+            if headers is not None:
+                h['Access-Control-Allow-Headers'] = headers
+            return resp
+
+        f.provide_automatic_options = False
+        return update_wrapper(wrapped_function, f)
+    return decorator
 
 def restart_comms_module():
     cmd = ['sudo python /home/pi/blockytalky/backend/comms_module.py']
